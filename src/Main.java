c0b20ebafc0810d61868bcffbb65ca4b18a5fc19
Darpyface <61155139+Darpyface@users.noreply.github.com>
2024-02-10 11:21:43 -0500

    Starting project

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.net.InetAddress;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        int clientPort = 5099;
+        int hostPort = 5098;
+        int serverPort = 5097;
+
+        InetAddress clientIP = InetAddress.getLoopbackAddress();
+        InetAddress hostIP = InetAddress.getLoopbackAddress();
+        InetAddress serverIP = InetAddress.getLoopbackAddress();
+
+
+    }
+}

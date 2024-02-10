5d365cd541da81fa1d52cb73c42e814996d92877
Darpyface <61155139+Darpyface@users.noreply.github.com>
2024-02-10 13:30:01 -0500

    Host Implementation

--- src/Host.java
+++ src/Host.java
@@ -1,6 +1,87 @@
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
 public class Host implements Runnable {
+
+    private final int bufferSize;
+    private final InetAddress serverIP;
+    private InetAddress clientIP;
+    private int clientPort;
+    private final int serverPort;
+    private final int hostClientPort;
+    private final int hostPort;
+    private DatagramPacket receivePacket;
+    private DatagramPacket sendPacket;
+    private DatagramSocket clientSocket;
+    private DatagramSocket sendSocket;
+
+    Host(int serverPort, int hostClientPort, int hostPort, int bufferSize, InetAddress serverIP) {
+        this.serverPort = serverPort;
+        this.hostClientPort = hostClientPort;
+        this.hostPort = hostPort;
+        this.bufferSize = bufferSize;
+        this.serverIP = serverIP;
+
+        try {
+            clientSocket = new DatagramSocket(hostClientPort);
+        } catch (Exception e) {
+            System.out.println("Cannot create Host Socket");
+            System.exit(1);
+        }
+        try {
+            sendSocket = new DatagramSocket(hostPort);
+        } catch (Exception e) {
+            System.out.println("Cannot create Host Socket");
+            System.exit(1);
+        }
+    }
 
     public void run() {
         System.out.println("Starting Host");
+        while (true) {
+            //Receive Packet from client
+            receivePacket = new DatagramPacket(new byte[bufferSize], bufferSize);
+            try {
+                clientSocket.receive(receivePacket);
+            } catch (Exception e) {
+                System.out.println("Error receiving packet on Host");
+            }
+
+            UDP.printPacketInfo(receivePacket); //Print info
+            clientIP = receivePacket.getAddress(); //Record its IP
+            clientPort = receivePacket.getPort(); //Record its port
+
+            //Create and send packet to Server
+            sendPacket = UDP.createPacket(UDP.packetDecode(receivePacket), bufferSize, UDP.extractMessage(receivePacket));
+            UDP.printPacketInfo(sendPacket);
+            try {
+                sendSocket.connect(serverIP, serverPort);
+                sendSocket.send(sendPacket);
+            } catch (Exception e) {
+                System.out.println("Could not send Packet to Server");
+                System.exit(1);
+            }
+
+            //Receive Response from server
+            receivePacket = new DatagramPacket(new byte[bufferSize], bufferSize);
+            try {
+                sendSocket.receive(receivePacket);
+            } catch (Exception e) {
+                System.out.println("Error receiving packet on Host");
+            }
+            UDP.printPacketInfo(receivePacket);
+
+            //Send response from server to client
+            sendPacket = UDP.createPacket(UDP.packetDecode(receivePacket), bufferSize, UDP.extractMessage(receivePacket));
+            UDP.printPacketInfo(sendPacket);
+            try {
+                sendSocket.connect(clientIP, clientPort);
+                sendSocket.send(sendPacket);
+            } catch (Exception e) {
+                System.out.println("Could not send packet to host");
+                System.exit(1);
+            }
+        }
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,8 @@ public class Main {
 
         int clientPort = 5099;
         int hostPort = 5098;
-        int serverPort = 5097;
+        int hostClientPort = 5097;
+        int serverPort = 5096;
 
         int bufferSize = 1024;
 
@@ -16,7 +17,7 @@ public class Main {
         InetAddress hostIP = InetAddress.getLoopbackAddress();
         InetAddress serverIP = InetAddress.getLoopbackAddress();
 
-        Client client = new Client(hostPort, clientPort, bufferSize, fileName, hostIP);
+        Client client = new Client(hostClientPort, clientPort, bufferSize, fileName, hostIP);
 
         Thread clientThread = new Thread(client);
         clientThread.start();
